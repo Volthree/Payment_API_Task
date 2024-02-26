34eac029d139a9958ea0f8f30d46af17ee04b4d0
Vladislav Maltsev <Vladiysvelikolepniy@gmail.com>
2024-02-26 16:29:29 +0300

    c1

---
+++ src/main/java/vladislavmaltsev/paymenttaskapi/PaymentTaskApiApplication.java
@@ -0,0 +1,13 @@
+package vladislavmaltsev.paymenttaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentTaskApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vladislavmaltsev/paymenttaskapi/PaymentTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vladislavmaltsev.paymenttaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentTaskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
